51a3d730e5d8445c452f278b8c039e973be06b59
AnkitBiswas1991 <ankitbiswas03@gmail.com>
2026-06-15 23:21:06 +0530

    uploaded full server data from git to https://render.com

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://aurora-voyages.onrender.com/',
         changeOrigin: true,
         secure: false,
       },
